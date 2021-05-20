064e4093c30588cf19ac3823309ef0d687bca808
Daniel B Kim <danielkim@Daniels-MBP.fios-router.home>
2021-05-20 18:42:31 -0400

    adds migration file

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+module.exports = {
+	development: {
+		client: "pg",
+		connection: "postgresql://postgres:D@niel12#@localhost:5432/music",
+		migrations: {
+			directory: "./db/migrations",
+		},
+		seeds: {
+			directory: "./db/seeds/dev",
+		},
+	},
+	test: {
+		client: "pg",
+		connection: "postgresql://postgres:D@niel12#@localhost:5432/music_testing",
+		migrations: {
+			directory: "./db/migrations",
+		},
+		seeds: {
+			directory: "./db/seeds/testing",
+		},
+	},
+	production: {
+		client: "pg",
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: "./db/migrations",
+		},
+	},
+};
